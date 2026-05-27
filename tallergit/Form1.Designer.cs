c59ff8efaee44a21800a23ebf98db937303348d1
Alexander <robles.alexander200605@gmail.com>
2026-05-26 19:31:06 -0700

    Cambiar fondo del formulario principal

--- tallergit/Form1.Designer.cs
+++ tallergit/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ButtonHighlight;
+            BackColor = Color.White;
             ClientSize = new Size(800, 450);
             Controls.Add(button1);
             Name = "Form1";
