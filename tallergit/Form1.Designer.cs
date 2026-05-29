7f14c31a9ffa675aade310b01b3e1a9d2aa88d1c
Alexander <robles.alexander200605@gmail.com>
2026-05-28 17:44:14 -0700

    'agregarimagen'

--- tallergit/Form1.Designer.cs
+++ tallergit/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(193, 175);
+            button1.Location = new Point(293, 248);
             button1.Name = "button1";
             button1.Size = new Size(299, 118);
             button1.TabIndex = 0;
@@ -41,20 +44,33 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(56, 28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(196, 149);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private PictureBox pictureBox1;
     }
 }
